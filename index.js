eb7ca0565b560764b1c9bda7e4b6ef2fe08dd010
ReTrn <84667504+wbaigei@users.noreply.github.com>
2022-06-21 15:05:44 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-// JavaScript for click and change image.
-var x = document.getElementById("x");
-var body = document.getElementById('clicker');
-var lcm = document.getElementById('lcm');
-var nums = document.getElementById('n');
+const x = document.getElementById("x");
+const body = document.getElementById('clicker');
+const lcm = document.getElementById('lcm');
+const nums = document.getElementById('n');
 var clicks = localStorage.getItem('LcmClicks');
-var clicker = document.getElementById('clicker');
-var rick = document.getElementById('rickRollV');
-var rickdiv = document.getElementById('rickRoll');
+const clicker = document.getElementById('clicker');
+const rick = document.getElementById('rickRollV');
+const rickdiv = document.getElementById('rickRoll');
+const html = document.getElementsByTagName('html')[0];
+var rickrolling = false;
 clicks = clicks == undefined ? 0 : clicks;
 nums.innerText = (clicks);
 body.onmousedown = () => {
-    clicks++;
-    nums.innerText = (clicks);
-    localStorage.setItem('LcmClicks', clicks);
-    lcm.src = './d1c7e543-f09f-47c7-97d6-f4f157a13d4c.png';
-}
-body.ontouchstart = body.onmousedown;
-body.onmouseup = () => {
-    lcm.src = './d1c7e543-f09f-47c7-97d6-f4f157a13d3c.png';
-    if (clicks % 100 == 68) {
-        clicker.style.display = 'none';
-        rickdiv.style.display = 'flex';
-        rick.play();
+    if (!rickrolling) {
+        localStorage.setItem('LcmClicks', clicks);
+        lcm.src = './d1c7e543-f09f-47c7-97d6-f4f157a13d4c.png';
     }
 }
-body.ontouchend = body.onmouseup;
+body.onmouseup = () => {
+    if (!rickrolling) {
+        lcm.src = './d1c7e543-f09f-47c7-97d6-f4f157a13d3c.png';
+        clicks++;
+        nums.innerText = (clicks);
+        if (clicks % 100 == 68) {
+            clicker.style.display = 'none';
+            rickdiv.style.display = 'flex';
+            nums.innerText = ("Rickrolled");
+            rickrolling = true;
+            rick.play();
+        }
+    }
+}
+html.onkeydown = body.ontouchstart = body.onmousedown;
+html.onkeyup = body.ontouchend = body.onmouseup;
 var closeX = () => {
     rickdiv.style.display = 'none';
     clicker.style.display = 'flex';
     rick.pause();
+    rickrolling = false;
 }
+document.getElementById('x').onclick = closeX;
